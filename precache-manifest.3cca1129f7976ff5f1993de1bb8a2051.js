0f9191d35de73104afba722291a7efd59df285ca
mlizchap <mlizchap@gmail.com>
2019-04-01 16:59:16 -0400

    Updates

---
+++ precache-manifest.3cca1129f7976ff5f1993de1bb8a2051.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "491cbce4f7bed1e5dcdf6f706e73a4bc",
+    "url": "/webdevPortfolio/static/media/samantha.491cbce4.woff"
+  },
+  {
+    "revision": "8b4ee5d85996de14800d",
+    "url": "/webdevPortfolio/static/css/main.da9d73b7.chunk.css"
+  },
+  {
+    "revision": "43392c9266ba6334c6d2",
+    "url": "/webdevPortfolio/static/js/runtime~main.3f590cd9.js"
+  },
+  {
+    "revision": "400b942d4dbd979fb32a",
+    "url": "/webdevPortfolio/static/js/2.17da54c8.chunk.js"
+  },
+  {
+    "revision": "23234e67a7f00d2fbbafe76575eff177",
+    "url": "/webdevPortfolio/static/media/arrow.23234e67.png"
+  },
+  {
+    "revision": "d22274e2fd95169f76783b2c42934b0a",
+    "url": "/webdevPortfolio/static/media/me.d22274e2.png"
+  },
+  {
+    "revision": "ee135d37113fbdf4b52a1d1081d90d90",
+    "url": "/webdevPortfolio/static/media/flower_left.ee135d37.png"
+  },
+  {
+    "revision": "dfbb559136483671049d30622766905c",
+    "url": "/webdevPortfolio/static/media/flower_right.dfbb5591.png"
+  },
+  {
+    "revision": "deb578cf37c35234e97c5cad1541c7e6",
+    "url": "/webdevPortfolio/static/media/flower_top_right.deb578cf.png"
+  },
+  {
+    "revision": "841b0479638e482e280cfc147406ad14",
+    "url": "/webdevPortfolio/static/media/flower_top_left.841b0479.png"
+  },
+  {
+    "revision": "8b4ee5d85996de14800d",
+    "url": "/webdevPortfolio/static/js/main.24b03cce.chunk.js"
+  },
+  {
+    "revision": "e48004f2c67d30bfc783fe40b748d4e1",
+    "url": "/webdevPortfolio/static/media/3dSketch.e48004f2.woff"
+  },
+  {
+    "revision": "ee369fa8241731fd36e648c617f2fab0",
+    "url": "/webdevPortfolio/static/media/arrowFont.ee369fa8.woff"
+  },
+  {
+    "revision": "dad943419e43ca59019b6528436c711b",
+    "url": "/webdevPortfolio/static/media/bird.dad94341.png"
+  },
+  {
+    "revision": "ee503503b73d33247b7a4587efaec487",
+    "url": "/webdevPortfolio/static/media/curl_right.ee503503.png"
+  },
+  {
+    "revision": "8983bb73a5969d04f79ea8ea1c8552e9",
+    "url": "/webdevPortfolio/static/media/curl_left.8983bb73.png"
+  },
+  {
+    "revision": "99cf6b233d08e0cf187fc3d1e56e19fb",
+    "url": "/webdevPortfolio/static/media/project1.99cf6b23.png"
+  },
+  {
+    "revision": "b3926f9422d1c67f5d91b1a7401ce602",
+    "url": "/webdevPortfolio/static/media/project2.b3926f94.png"
+  },
+  {
+    "revision": "d235848325f8556c0908193675f44184",
+    "url": "/webdevPortfolio/static/media/project1_selected.d2358483.png"
+  },
+  {
+    "revision": "8892e8c2df12a208b8d6d34e63cf792e",
+    "url": "/webdevPortfolio/static/media/project2_selected.8892e8c2.png"
+  },
+  {
+    "revision": "599f9abb1801f072702edf2b9b308dfb",
+    "url": "/webdevPortfolio/index.html"
+  }
+];
